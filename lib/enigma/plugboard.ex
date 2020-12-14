a388af94695e4f5ce1c9dbf587751cb5f4daca3e
Richard Bickford <rzacharski05@gmail.com>
2020-12-14 16:04:21 -0500

    plugboard implemented

--- lib/enigma/plugboard.ex
+++ lib/enigma/plugboard.ex
@@ -1,10 +1,22 @@
 defmodule Enigma.Plugboard do
-  def transform(_single_character_input, _plugboard_config) do
-    # take the given character and swap it according to how the plugboard is configured
-    # returns the same character if it is not configured
+  # If the plugboard is not configured it should just be a passthrough
+  def transform(single_character_input, %Enigma.State{plugboard: nil}) do
+    single_character_input
   end
 
-  def setup_plugboard(options \\ []) do
-    options
+  def transform(single_character_input, %Enigma.State{plugboard: plugboard_config}) do
+    # Check each configured plugboard swap to see if it contains the input character
+    Enum.find(plugboard_config, &String.contains?(&1, single_character_input))
+    |> process_result(single_character_input)
+  end
+
+  # If there is no match with the current plugboard config, return the given letter
+  defp process_result(nil, single_character_input) do
+    single_character_input
+  end
+
+  # If there is a match, give the other letter in the given plugboard swap
+  defp process_result(plugboard_string, single_character_input) do
+    String.trim(plugboard_string, single_character_input)
   end
 end
