5458b6b808f8e0f62ed2943a1297cf2ea4cd5d3b
Richard Bickford <rzacharski05@gmail.com>
2020-12-14 16:15:44 -0500

    implemented reflector

--- lib/enigma/reflector.ex
+++ lib/enigma/reflector.ex
@@ -1,2 +1,11 @@
 defmodule Enigma.Reflector do
+  @alphabet ~w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+
+  def transform(single_character_input, %Enigma.State{reflector: reflector}) do
+    reflector_index =
+      String.codepoints(reflector)
+      |> Enum.find_index(&String.contains?(&1, single_character_input))
+
+    Enum.fetch!(@alphabet, reflector_index)
+  end
 end
